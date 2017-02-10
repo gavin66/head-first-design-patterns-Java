3fbf729959d00cc223d5bcca93360bac105310d1
Gavin <tianyu_0723@hotmail.com>
2017-02-10 18:24:32 +0800

    model duck 继承基础 duck

---
+++ src/headfirst/strategy/ModelDuck.java
@@ -0,0 +1,16 @@
+package headfirst.strategy;
+
+/**
+ * Created by Gavin on 2017/2/10.
+ */
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a model duck");
+    }
+}
