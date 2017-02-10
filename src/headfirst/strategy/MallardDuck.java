2826b95ce2c47df4daf276e11dcac6ba9a5060d4
Gavin <tianyu_0723@hotmail.com>
2017-02-10 18:24:10 +0800

    mallard duck ，继承基础duck

---
+++ src/headfirst/strategy/MallardDuck.java
@@ -0,0 +1,17 @@
+package headfirst.strategy;
+
+/**
+ * Created by Gavin on 2017/2/10.
+ */
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a real Mallard duck");
+    }
+}
