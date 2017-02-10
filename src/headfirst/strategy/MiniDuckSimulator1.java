579298268e56457f3920528f6ce9caf6a4ecf4c0
Gavin <tianyu_0723@hotmail.com>
2017-02-10 18:25:06 +0800

    测试类

---
+++ src/headfirst/strategy/MiniDuckSimulator1.java
@@ -0,0 +1,17 @@
+package headfirst.strategy;
+
+/**
+ * Created by Gavin on 2017/2/10.
+ */
+public class MiniDuckSimulator1 {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+    }
+}
