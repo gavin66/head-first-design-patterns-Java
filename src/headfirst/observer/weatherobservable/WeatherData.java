69ed661493635df3a9b443a3d7b45ec707cf60c8
Gavin <tianyu_0723@hotmail.com>
2017-02-13 17:32:33 +0800

    继承Java内置可观察的类

---
+++ src/headfirst/observer/weatherobservable/WeatherData.java
@@ -0,0 +1,40 @@
+package headfirst.observer.weatherobservable;
+
+import java.util.Observable;
+
+/**
+ * Created by Gavin on 2017/2/13.
+ */
+public class WeatherData extends Observable {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+
+    }
+
+    public void measurementsChanged(){
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+}
