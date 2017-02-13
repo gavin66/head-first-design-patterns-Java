9d27e21af86bfe6e98c0f380e37ca65a1c66528b
Gavin <tianyu_0723@hotmail.com>
2017-02-13 17:31:30 +0800

    共用部分提炼成接口

---
+++ src/headfirst/observer/weather/DisplayElement.java
@@ -0,0 +1,8 @@
+package headfirst.observer.weather;
+
+/**
+ * Created by Gavin on 2017/2/13.
+ */
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/headfirst/observer/weatherobservable/DisplayElement.java
@@ -0,0 +1,8 @@
+package headfirst.observer.weatherobservable;
+
+/**
+ * Created by Gavin on 2017/2/13.
+ */
+public interface DisplayElement {
+    public void display();
+}
