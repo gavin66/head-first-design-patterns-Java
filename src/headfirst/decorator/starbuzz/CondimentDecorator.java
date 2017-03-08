28facdbc5ab0d9502fadfdfe044204ee97a98d4d
Gavin <tianyu_0723@hotmail.com>
2017-03-08 14:39:36 +0800

    装饰者的底层接口，装饰者都需要实现

---
+++ src/headfirst/decorator/starbuzz/CondimentDecorator.java
@@ -0,0 +1,10 @@
+package headfirst.decorator.starbuzz;
+
+/**
+ * Created by Gavin on 2017/3/7.
+ */
+public abstract class CondimentDecorator extends Beverage {
+
+    public abstract String getDescription();
+
+}
