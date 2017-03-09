0f202bd58a6aa2133d2a64e408470c2a9838902c
Gavin <tianyu_0723@hotmail.com>
2017-03-09 18:30:57 +0800

    抽象工厂，定义一组生产产品的接口

---
+++ src/headfirst/factory/pizzaaf/PizzaIngredientFactory.java
@@ -0,0 +1,18 @@
+package headfirst.factory.pizzaaf;
+
+/**
+ * Created by Gavin on 2017/3/9.
+ */
+public interface PizzaIngredientFactory {
+    Dough createDough();
+
+    Sauce createSauce();
+
+    Cheese createCheese();
+
+    Veggies[] createVeggies();
+
+    Pepperoni createPepperoni();
+
+    Clams createClam();
+}
