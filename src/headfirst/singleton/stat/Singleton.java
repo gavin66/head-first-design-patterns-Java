764443dd4e8b13bbd4544c9aa309e4b98d227685
Gavin <tianyu_0723@hotmail.com>
2017-03-13 15:41:07 +0800

    饿汉式单例

---
+++ src/headfirst/singleton/stat/Singleton.java
@@ -0,0 +1,19 @@
+package headfirst.singleton.stat;
+
+/**
+ * Created by Gavin on 2017/3/13.
+ */
+public class Singleton {
+    private static Singleton uniqueInstance = new Singleton();
+
+    public Singleton() {
+    }
+
+    public static Singleton getInstance(){
+        return uniqueInstance;
+    }
+
+    public String getDescription(){
+        return "I'm a statically initialized Singleton!";
+    }
+}
---
+++ src/headfirst/singleton/stat/SingletonClient.java
@@ -0,0 +1,11 @@
+package headfirst.singleton.stat;
+
+/**
+ * Created by Gavin on 2017/3/13.
+ */
+public class SingletonClient {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}
