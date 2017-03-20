aff27ed724b961857f4fa817fce7da2fead4c2e4
Gavin <tianyu_0723@hotmail.com>
2017-03-20 11:31:46 +0800

    swing 窗口程序

---
+++ src/headfirst/templatemethod/frame/MyFrame.java
@@ -0,0 +1,29 @@
+package headfirst.templatemethod.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Gavin on 2017/3/20.
+ */
+public class MyFrame extends JFrame{
+    private static final long serialVersionUID = 2L;
+
+    public MyFrame(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setSize(300,300);
+        this.setVisible(true);
+    }
+
+    public void paint(Graphics graphics){
+        super.paint(graphics);
+        String msg = "I rule!!";
+        graphics.drawString(msg,100,100);
+    }
+
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame("Head First Design Patterns");
+    }
+}
