4fe356b32d16e86c3dd06529f34f221475be4a24
Gavin <tianyu_0723@hotmail.com>
2017-03-20 10:50:40 +0800

    排序模板方法的实现

---
+++ src/headfirst/templatemethod/sort/Duck.java
@@ -0,0 +1,32 @@
+package headfirst.templatemethod.sort;
+
+/**
+ * Created by Gavin on 2017/3/20.
+ */
+public class Duck implements Comparable<Duck> {
+    String name;
+    int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " weighs " + weight;
+    }
+
+    @Override
+    public int compareTo(Duck object) {
+        Duck otherDuck = object;
+
+        if (this.weight < otherDuck.weight) {
+            return -1;
+        } else if (this.weight == otherDuck.weight) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
